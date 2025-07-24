6eb5dd8bdb05a2417b6c58a924a525cfb018657f
tanim246 <mdtasnimhossaintanim@gmail.com>
2025-07-24 13:58:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const addNote = () => {
+    const noteInput = document.getElementById("noteInput")
+    const noteText = noteInput.value
+    console.log(noteText);
+    noteInput.value = ''; //  input value clear
+    if (noteText === "") {
+        alert("Please enter a note.");
+        return;
+    }
+    // li tag js code
+    const li = document.createElement("li") // create li tag 
+    // li.innerText = noteText
+    li.innerHTML = `
+    <span>${noteText}</span>
+    <div class="twoButton">
+        <button class="edit" onclick="editNote(this)">Edit</button>
+        <button class="delate" onclick="delateNote(this)">Delate</button>
+     </div>`
+    document.getElementById("noteList").appendChild(li);
+};
+// delate button
+function delateNote(data){
+    const li = data.closest("li")
+    li.remove();
+}
+
